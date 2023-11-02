2ed251a3ca05602950af7b8fd57c8b327ccd4890
Shoobi <123006808+WShoobi@users.noreply.github.com>
2023-11-02 10:37:13 -0700

    Update visitor_counts.php

--- visitor_counts.php
+++ visitor_counts.php
@@ -1,1 +1,12 @@
+<?php
+// Simulating the visitor counts data
+$visitorCounts = array(
+  "today" => 50,
+  "thisWeek" => 350,
+  "thisMonth" => 1500
+);
 
+// Returning the visitor counts as JSON response
+header('Content-Type: application/json');
+echo json_encode($visitorCounts);
+?>
